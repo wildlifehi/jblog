871bc72e8039eab1393812ed853af269a5ce68d7
청출어람_PC14 <_PC14@DESKTOP-OT013FC>
2022-06-17 16:15:15 +0900

    jblog update

---
+++ docs/jblog.sql
@@ -0,0 +1,6 @@
+-- scheme
+show tables;
+desc user;
+desc blog;
+desc category;
+desc post;
---
+++ jblog03/src/main/java/com/douzone/jblog/controller/BlogController.java
@@ -0,0 +1,38 @@
+package com.douzone.jblog.controller;
+
+import java.util.Optional;
+
+@RequestMapping("/{id:(?!assets.*)}") // 중복되는거 맨 위로 올리고
+public class BlogController {
+	
+	// 여기서 어드민까지 전부 처리할 것.
+	// 컨트롤러는 더 이상 만들지 말고 여기서 다 처리하도록
+	
+	//@RequestMapping({"", "/{categoryNo}", "/{categoryNo}/{postNo}"}) 
+	@RequestMapping({"", "/{pathNo1}", "/{pathNo1}/{pathNo2}"}) 
+	//들어오는 방법은 3가지이므로 3가지 멀티매핑으로 설정
+	public String index(
+			@PathVariable("id") String id, 
+			@PathVariable("pathNo1") Optional<Long> pathNo1,
+			@PathVariable("pathNo1") Optional<Long> pathNo2) {
+				
+			Long categoryNo = 0L;
+			Long postNo = 0L;
+			if(pathNo1.isPresent()) {
+				categoryNo = pathNo1.get();
+			} else if(pathNo1.isPresent()) {
+				
+			}
+			
+			categoryService.getCategories(id, categoryNo);
+			
+	}
+
+	@RequestMapping("/{id}")
+	public String adminBasic(@PathVariable("id") String id) {
+		
+	}
+	
+	
+	
+}
