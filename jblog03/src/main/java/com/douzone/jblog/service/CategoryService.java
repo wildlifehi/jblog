f0aa445cd3466ac6d606674b891f3da01de89e48
wildlifehi <wildlifehi@gamil.com>
2022-06-25 13:56:12 +0900

    update

--- jblog03/src/main/java/com/douzone/jblog/service/CategoryService.java
+++ jblog03/src/main/java/com/douzone/jblog/service/CategoryService.java
@@ -1,14 +1,24 @@
 package com.douzone.jblog.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.douzone.jblog.repository.CategoryRepository;
+import com.douzone.jblog.vo.CategoryVo;
 import com.douzone.jblog.vo.UserVo;
 
 @Service
 public class CategoryService {
 
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	public void join(UserVo vo) {
-		userRepository.insert(vo);
+		
+		CategoryVo categoryVo = new CategoryVo();
+		categoryVo.setBlogId(vo.getId());
+		
+		categoryRepository.insert(categoryVo);
 		
 	}
 }
